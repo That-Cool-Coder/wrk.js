5f86f8daf96ee1a3a96b2b027a24245787fa84d6
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2020-08-24 18:19:17 +0930

    abbreviated vector to v

--- src/vector.js
+++ src/vector.js
@@ -1,86 +1,86 @@
-wrk.vector = function(x, y, z=0) {
+wrk.v = function(x, y, z=0) {
     // simple and (hopefully) fast
     return {x : x, y : y, z : z};
 }
 
-wrk.vector.random = function(min, max, floatsAllowed=true) {
+wrk.v.random = function(min, max, floatsAllowed=true) {
     if (floatsAllowed) {
-        return new wrk.vector(wrk.randflt(min.x, max.y),
+        return new wrk.v(wrk.randflt(min.x, max.y),
             wrk.randflt(min.y, max.y),
             wrk.randflt(min.z, max.z));
     }
     else {
-        return new wrk.vector(wrk.randint(min.x, max.y),
+        return new wrk.v(wrk.randint(min.x, max.y),
             wrk.randint(min.y, max.y),
             wrk.randint(min.z, max.z));
     }
 }
 
-wrk.vector.copy = function(v) {
-    return wrk.vector(v.x, v.y, v.z);
+wrk.v.copy = function(v) {
+    return wrk.v(v.x, v.y, v.z);
 }
 
-wrk.vector.add = function(v1, v2) {
+wrk.v.add = function(v1, v2) {
     v1.x += v2.x;
     v1.y += v2.y;
     v1.z += v2.z;
 }
 
-wrk.vector.copyAdd = function(v1, v2) {
-    var v3 = wrk.vector.copy(v1);
-    wrk.vector.add(v3, v2);
+wrk.v.copyAdd = function(v1, v2) {
+    var v3 = wrk.v.copy(v1);
+    wrk.v.add(v3, v2);
     return v3;
 }
 
-wrk.vector.sub = function(v1, v2) {
+wrk.v.sub = function(v1, v2) {
     v1.x -= v2.x;
     v1.y -= v2.y;
     v1.z -= v2.z;
 }
 
-wrk.vector.copySub = function(v1, v2) {
-    var v3 = wrk.vector.copy(v1);
-    wrk.vector.sub(v3, v2);
+wrk.v.copySub = function(v1, v2) {
+    var v3 = wrk.v.copy(v1);
+    wrk.v.sub(v3, v2);
     return v3;
 }
 
-wrk.vector.mult = function(v, amount) {
+wrk.v.mult = function(v, amount) {
     v.x *= amount;
     v.y *= amount;
     v.z *= amount;
 }
 
-wrk.vector.copyMult = function(v, amount) {
-    var v2 = wrk.vector.copy(v);
-    wrk.vector.mult(v2, amount);
+wrk.v.copyMult = function(v, amount) {
+    var v2 = wrk.v.copy(v);
+    wrk.v.mult(v2, amount);
     return v2;
 }
 
-wrk.vector.div = function(v, amount) {
+wrk.v.div = function(v, amount) {
     v.x /= amount;
     v.y /= amount;
     v.z /= amount;
 }
 
-wrk.vector.copyDiv = function(v, amount) {
-    var v2 = wrk.vector.copy(v);
-    wrk.vector.div(v2, amount);
+wrk.v.copyDiv = function(v, amount) {
+    var v2 = wrk.v.copy(v);
+    wrk.v.div(v2, amount);
     return v2;
 }
 
-wrk.vector.magSq = function(v) {
+wrk.v.magSq = function(v) {
     return v.x ** 2 + v.y ** 2 + v.z ** 2;
 }
 
-wrk.vector.mag = function(v) {
-    return wrk.sqrt(wrk.vector.magSq(v));
+wrk.v.mag = function(v) {
+    return wrk.sqrt(wrk.v.magSq(v));
 }
 
-wrk.vector.distSq = function(v1, v2) {
-    var v3 = wrk.vector.copySub(v2, v1);
-    return wrk.vector.magSq(v3);
+wrk.v.distSq = function(v1, v2) {
+    var v3 = wrk.v.copySub(v2, v1);
+    return wrk.v.magSq(v3);
 }
 
-wrk.vector.dist = function(v1, v2) {
-    return wrk.sqrt(wrk.vector.distSq(v1, v2));
+wrk.v.dist = function(v1, v2) {
+    return wrk.sqrt(wrk.v.distSq(v1, v2));
 }
