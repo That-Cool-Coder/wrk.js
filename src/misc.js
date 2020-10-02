e5e787f6305756d9d4f59740d45f3b21feee06d4
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2020-10-02 14:10:33 +0930

    Documentation work

--- src/misc.js
+++ src/misc.js
@@ -2,11 +2,11 @@ wrk.uniqueId = function() {
     // Math.random should be unique because of its seeding algorithm.
     // Convert it to base 36 (numbers + letters), and grab the first 9 characters
     // after the decimal.
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return '_' + wrk.random().toString(36).substr(2, 9);
 }
 
 wrk.randBoolean = function() {
-    return wrk.randflt(0, 1) > 0.5;
+    return wrk.random() > 0.5;
 }
 
 wrk.doNothing = function() {
@@ -16,9 +16,7 @@ wrk.doNothing = function() {
 wrk.constrain = function(num, min, max) {
     // Constrain num between min and max
 
-    if (num < min) num = min;
-    if (num > max) num = max;
-    return num;
+    return Math.max(min, Math.min(num, max))
 }
 
 wrk.wrapAround = function(num, min, max) {
