18d4cfd8d3467b72cb957d4255b020fc9067c842
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2020-11-24 18:30:51 +1030

    mouseWatcher has bools on mouse state

--- wrk.js
+++ wrk.js
@@ -685,11 +685,39 @@ wrk.MouseWatcher = class {
         this.elem = elem;
 
         this.position = wrk.v(0, 0);
+
+        this.pointerDown = false;
+        this.mouseDown = false;
+        this.touchDown = false;
         
         this.elem.addEventListener('mousemove', e => {
             var rect = e.target.getBoundingClientRect();
             this.position.x = e.x - rect.left;
             this.position.y = e.y - rect.top;
+        });
+
+        this.elem.addEventListener('mousedown', e => {
+            this.mouseDown = true;
+        });
+
+        this.elem.addEventListener('mouseup', e => {
+            this.mouseDown = false;
+        });
+
+        this.elem.addEventListener('touchstart', e => {
+            this.touchDown = true;
+        });
+
+        this.elem.addEventListener('touchend', e => {
+            this.touchDown = false;
+        });
+
+        this.elem.addEventListener('pointerdown', e => {
+            this.pointerDown = true;
+        });
+
+        this.elem.addEventListener('pointerup', e => {
+            this.pointerDown = false;
         });
     }
 }
